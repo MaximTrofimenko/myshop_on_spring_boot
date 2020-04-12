4a8d6249ec65059698e0dd1250deeedb97556450
Maxim Trofimenko <oklickk@yandex.ru>
2020-04-12 14:16:38 +0300

    модернизировал сущность добавил - новые связи

--- src/main/java/com/trofimenko/myshop/persistence/entities/Purchase.java
+++ src/main/java/com/trofimenko/myshop/persistence/entities/Purchase.java
@@ -3,7 +3,8 @@ package com.trofimenko.myshop.persistence.entities;
 import com.trofimenko.myshop.persistence.entities.utils.PersistableEntity;
 import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @Entity
@@ -11,4 +12,17 @@ public class Purchase extends PersistableEntity {
     private Double price;
     private String address;
     private String phone;
+
+    @OneToMany(mappedBy = "purchase", cascade = CascadeType.ALL)
+    private List<CartRecord> cartRecords;
+
+    @ManyToOne
+    @JoinColumn(name = "shopuser")
+    private Shopuser shopuser;
+
+    @ManyToMany
+    @JoinTable(name="purchase_product", joinColumns=
+    @JoinColumn(name="purchase", referencedColumnName="id"), inverseJoinColumns=
+    @JoinColumn(name="product", referencedColumnName="id"))
+    private List<Product> products;
 }
